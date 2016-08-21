d4da953ba70bfaf5aea124363c9a64f75827f00c
yenashenakhte2016 <mmnsfyrad@gmail.com>
2016-08-22 01:15:22 +0430

    Add files via upload

--- plugins/InSuper.lua
+++ plugins/InSuper.lua
@@ -778,12 +778,246 @@ end
 			data[tostring(target)]['settings']['lock_fwd'] = 'no'
 		end
 	end
+     local expiretime = redis:hget('expiretime', get_receiver(msg))
+    local expire = ''
+  if not expiretime then
+  expire = expire..'Expire NotSet'
+  else
+   local now = tonumber(os.time())
+   expire =  expire..math.floor((tonumber(expiretime) - tonumber(now)) / 86400) + 1
+ end
   local settings = data[tostring(target)]['settings']
-  local text = "SuperGroup settings:\n\nLock Links > "..settings.lock_link.."\nLock Webpage > "..settings.lock_webpage.."\nLock Tag > "..settings.lock_tag.."\nLock Emoji > "..settings.lock_emoji.."\nLock English > "..settings.lock_eng.."\nLock Badword > "..settings.lock_badw.."\nLock Flood > "..settings.flood.."\nFlood sensitivity > "..NUM_MSG_MAX.."\nLock Spam > "..settings.lock_spam.."\nLock Contacts > "..settings.lock_contacts.."\nLock Arabic/Persian > "..settings.lock_arabic.."\nLock Member > "..settings.lock_member.."\nLock RTL > "..settings.lock_rtl.."\nLock Forward > "..settings.lock_fwd.."\nLock TGservice > "..settings.lock_tgservice.."\nLock Sticker > "..settings.lock_sticker.."\nPublic > "..settings.public.."\nStrict Settings > "..settings.strict
+  local text = "SuperGroup settings:\n\nLock Links > "..settings.lock_link.."\nLock Webpage > "..settings.lock_webpage.."\nLock Tag > "..settings.lock_tag.."\nLock Emoji > "..settings.lock_emoji.."\nLock English > "..settings.lock_eng.."\nLock Badword > "..settings.lock_badw.."\nLock Flood > "..settings.flood.."\nFlood sensitivity > "..NUM_MSG_MAX.."\nLock Spam > "..settings.lock_spam.."\nLock Contacts > "..settings.lock_contacts.."\nLock Arabic/Persian > "..settings.lock_arabic.."\nLock Member > "..settings.lock_member.."\nLock RTL > "..settings.lock_rtl.."\nLock Forward > "..settings.lock_fwd.."\nLock TGservice > "..settings.lock_tgservice.."\nLock Sticker > "..settings.lock_sticker.."\nPublic > "..settings.public.."\nStrict Settings > "..settings.strict.."\nExpire Date > "..expire.."\nMute Documents: "..(redis:get("mute:document"..target) or 'no')..""
+.."\n#Silent Contacts: "..(redis:get("mute:contact"..target) or 'no').."" 
+.."\n#Silent Photo: "..(redis:get("mute:photo"..target) or "no")..""
+--.."\nMute Video: "..(redis:get("mute:video"..target) or 'no')..""
+.."\n#Silent Text: "..(redis:get("mute:text"..target) or 'no')..""
+.."\n#Silent Gifs: "..(redis:get("mute:gif"..target) or 'no')..""
+.."\n#Silent Audio: "..(redis:get("mute:audio"..target) or 'no').."" 
+.."\n#Silent All: "..(redis:get("mute:all"..target) or "no")
   local text = string.gsub(text,'yes','🔐')
   local text = string.gsub(text,'no','🔓')
   reply_msg(msg.id, text, ok_cb, false)
 end
+
+ local function muteuser(user_id, channel_id)
+  local hash = 'mute:'..channel_id
+  redis:sadd(hash, user_id)
+end
+local function is_mute(user_id, channel_id)
+  local hash = 'mute:'..channel_id
+  local muted = redis:sismember(hash, user_id)
+  return muted or false
+end
+local function muteuser_by_reply(extra, success, result)
+  vardump(result)
+    if result.to.peer_type == "channel" then
+		local channel = "channel#"..result.to.peer_id
+		if tonumber(result.from.peer_id) == tonumber(our_id) then -- Ignore bot
+			return "I won't silent myself"
+		end
+		local name = user_print_name(result.from)
+		local hash = 'mute:'..result.to.peer_id
+		if is_mute(result.from.peer_id, result.to.peer_id) then
+			send_large_msg(channel, name.." ["..result.from.peer_id.."] alowed to chat.")
+			redis:srem(hash, result.from.peer_id)
+		else
+			send_large_msg(channel, name.." ["..result.from.peer_id.."] not alowed to chat.")
+			redis:sadd(hash, result.from.peer_id)
+		end
+	end
+end
+
+   local function clean_mute(channel_id)
+  local hash = 'mute:'..channel_id
+  local list = redis:keys(hash)
+  for k,v in pairs(list) do
+    redis:srem(hash, v)
+  end
+  return "Mutelist Cleaned"
+end
+local function mute_list(channel_id)
+  local hash =  'mute:'..channel_id
+  local list = redis:keys(hash)
+  local text = "Muted list :\n\n"
+  local i = 1
+  for k,v in pairs(list) do
+    text = text..k.." - "..v.." \n"
+  end
+  return text
+end
+local function mute_res(extra, success, result)
+    local member_id = result.peer_id
+	local member = result.username
+	local channel_id = extra.channel_id
+	local mute_cmd = extra.mute_cmd
+	local receiver = "channel#id"..channel_id
+	if mute_cmd == "muteuser" then
+	    if is_mute(member_id, channel_id) then
+			send_large_msg(receiver, "["..member_id.."] removed from muted user list")
+			local hash = 'mute:'..channel_id
+			redis:srem(hash, member_id)
+		else
+			send_large_msg(receiver, "["..member_id.."] added to muted user list")
+			return muteuser(member_id, channel_id)
+		end
+	end
+end
+local function run(msg, matches)
+  local target = msg.to.id
+  if matches[1] == "muteuser" and is_momod(msg) then
+    if type(msg.reply_id)~= "nil" then
+	  msgreply = get_message(msg.reply_id, muteuser_by_reply, false)
+	end
+	if string.match(matches[2], '^%d+$') then
+	  local user_id = matches[2]
+	  local channel_id = msg.to.id
+	  muteuser(user_id, channel_id)
+	else
+	  local cb_extra = {
+	    channel_id = msg.to.id,
+        mute_cmd = 'muteuser',
+	  }
+	  local username = matches[2]
+	  local username = string.gsub(matches[2], "@", "")
+	  resolve_username(username, mute_res, cb_extra)
+	end
+  end
+  if matches[1] == "mutelist" then
+    local channel_id = msg.to.id
+	return mute_list(channel_id)
+  end
+  if matches[1] == "silent" and is_momod(msg) then
+    if matches[2]:lower() == "audio" then
+      redis:set("mute:audio"..target, "yes")
+      return "Silent Audio has been enabled"
+    end
+    if matches[2]:lower(0) == "text" then
+      redis:set("mute:text"..target, "yes")
+      return "Silent Text has been enabled"
+    end
+    --if matches[2]:lower() == "video" then
+      --redis:set("mute:video"..target, "yes")
+      --return "Mute Video has been enabled"
+    --end
+    if matches[2]:lower() == "photo" then 
+      redis:set("mute:photo"..target, "yes")
+      return "Silent Photo has been enabled"
+    end
+    if matches[2]:lower() == "all" then
+      redis:set("mute:all"..target, "yes")
+      return "Silent All has been enabled"
+    end
+	if matches[2]:lower() == "gifs" then
+	  redis:set("mute:gif"..target, "yes")
+	  return "Gifs sharing has been enabled"
+	end
+	if matches[2]:lower() == "contacts" then
+	  redis:set("mute:contact"..target, "yes")
+	  return "Share contact has been else"
+	end
+	--if matches[2]:lower() == "document" then
+	  --redis:set("mute:document"..target, "yes")
+	  --return "Mute Documents has been enabled"
+	--end
+  end
+  if matches[1] == "unsilent" and is_momod(msg) then
+    if matches[2]:lower() == "audio" then
+      redis:del("mute:audio"..msg.to.id)
+      return "Silent Audio has been disabled"
+    end
+    if matches[2]:lower() == "text" then
+      redis:del("mute:text"..msg.to.id)
+      return "Silent Text has been disabled"
+    end
+    if matches[2]:lower() == "photo" then
+      redis:del("mute:photo"..msg.to.id)
+      return "Silent Photo has been disabled"
+    end
+    --if matches[2]:lower() == "video" then
+      --redis:del("mute:video"..msg.to.id)
+      --return "Mute Video has been disabled"
+    --end
+    if matches[2]:lower() == "all" then
+      redis:del("mute:all"..msg.to.id)
+      return "Silent All has been disabled"
+    end
+	if matches[2]:lower() == "gifs" and is_momod(msg) then
+	  redis:del("mute:gif"..target)
+	  return "Gifs sharing has been disabled"
+	end
+	if matches[2]:lower() == "contacts" and is_momod(msg) then
+	  redis:del("mute:contact"..target)
+	  return "Share contacts has been disabled"
+	end
+	--if matches[2]:lower() == "document" then
+	 -- redis:del("mute:document"..target)
+	 -- return "Mute Documents has been disabled"
+	--end
+  end
+  if matches[1] == "silentlist" and is_momod(msg) then
+    return show_mute(msg, target)
+  end
+  if matches[1] == "clean" then
+    local channel_id = msg.to.id
+    return clean_mute(channel_id)
+  end
+end
+local function pre_process(msg)
+  if msg.service then
+    return msg
+  end
+  if redis:get("mute:all"..msg.to.id) and not is_momod(msg) then
+    delete_msg(msg.id, ok_cb, true)
+  end
+  if msg.text then
+    if redis:get("mute:text"..msg.to.id) == "yes" then
+      delete_msg(msg.id, ok_cb, false)
+    end
+  end
+  if msg.media then
+    if msg.media.type == "photo" then
+      if redis:get("mute:photo"..msg.to.id) == "yes" then
+        delete_msg(msg.id, ok_cb, false)
+      end
+    end
+  end
+  if msg.media then 
+    if msg.media.type == "audio" then
+	  if redis:get("mute:audio"..msg.to.id) == "yes" and not is_momod(msg) then
+	    delete_msg(msg.id, ok_cb, true)
+	  end
+	end
+  end
+  if msg.media then
+    if msg.media.type == "video" then
+      if redis:get("mute:video"..msg.to.id) == "yes" then
+        delete_msg(msg.id, ok_cb, false)
+      end
+    end
+  end
+  if msg.media then
+    if msg.media.caption == "giphy.mp4" and not is_momod(msg) then
+	  if redis:get("mute:gif"..msg.to.id) == "yes" then
+	    delete_msg(msg.id, ok_cb, true)
+	  end
+	end
+	if msg.media.type == "contact" and not is_momod(msg) then
+	  if redis:get("mute:contact"..msg.to.id) == "yes" then
+	    delete_msg(msg.id, ok_cb, true)
+	  end
+	end
+  end
+  if msg.media then
+    if msg.media.type == "document" and not msg.media.caption and not is_momod(msg) then
+	  if redis:get("mute:document"..msg.to.id) == "yes" then
+	    delete_msg(msg.id, ok_cb, true)
+	  end
+	end
+  end
+  if is_mute(msg.from.id, msg.to.id) then
+    delete_msg(msg.id, ok_cb, true)
+  end
 
 local function promote_admin(receiver, member_username, user_id)
   local data = load_data(_config.moderation.data)
